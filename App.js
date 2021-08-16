b9d6656c0c3a9e94b3a01034cb050312650fbfa9
lemosandre <lemosandrecosta@gmail.com>
2021-08-16 14:55:23 -0400

    Update code

--- App.js
+++ App.js
@@ -1,11 +1,49 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-
+import { StatusBar } from "expo-status-bar";
+import React, { useState } from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  ImageBackground,
+  TextInput,
+} from "react-native";
+import BackgroundImage from "./assets/shoe-print.png";
+import { shoes } from "./ShoesTable";
 export default function App() {
+  const [text, setText] = useState("");
+
+  const textChange = (text) => {
+    setText(text);
+    console.log(shoes.man);
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <ImageBackground
+        source={BackgroundImage}
+        resizeMode="contain"
+        style={styles.image}
+        imageStyle={{ opacity: 0.7 }}
+      >
+        <View
+          style={{
+            height: "70%",
+            backgroundColor: "red",
+            justifyContent: "space-evenly",
+            alignItems: "center",
+            paddingBottom: 100,
+          }}
+        >
+          <Text>Tamanho Do Seu Pe nos EUA</Text>
+          <TextInput
+            style={{ height: 70 }}
+            placeholder="Tamanho do seu Pe"
+            onChangeText={textChange}
+            defaultValue={text}
+          />
+          <Text>{text}</Text>
+        </View>
+      </ImageBackground>
       <StatusBar style="auto" />
     </View>
   );
@@ -14,8 +52,16 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#D2B48C",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  image: {
+    flex: 1,
+    width: "100%",
+    height: "100%",
+    justifyContent: "center",
+    alignItems: "center",
+    aspectRatio: 1.5,
   },
 });
